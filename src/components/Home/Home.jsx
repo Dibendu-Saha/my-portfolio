471390a7a218dbe3d35bc8bf72a694ae632cdcbe
Dibendu Saha <dibendu4@gmail.com>
2022-03-21 01:04:25 +0530

    Indentation 2 --> 4

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,27 +2,27 @@ import { Button } from "../../utils/Button";
 import "./Home.css";
 
 function Home() {
-  return (
-    <section className="section-home">
-      <div className="container-home">
-        <div className="grid grid--intro">
-          <div className="intro-me">
-            <h1>Hey there!</h1>
-            <p>I'm Dibendu</p>
-          </div>
-          <div className="flex flex--hobbies grid--intro-about">
-            <p>...and I'm a</p>
-            <p>Full Stack Developer</p>
-            <p>by profession</p>
-          </div>
-        </div>
+    return (
+        <section className="section-home">
+            <div className="container-home">
+                <div className="grid grid--intro">
+                    <div className="intro-me">
+                        <h1>Hey there!</h1>
+                        <p>I'm Dibendu</p>
+                    </div>
+                    <div className="flex flex--hobbies grid--intro-about">
+                        <p>...and I'm a</p>
+                        <p>Full Stack Developer</p>
+                        <p>by profession</p>
+                    </div>
+                </div>
 
-        <div className="explore">
-          <Button title="Let's Explore" type="primary" />
-        </div>
-      </div>
-    </section>
-  )
+                <div className="explore">
+                    <Button title="Let's Explore" type="primary" />
+                </div>
+            </div>
+        </section>
+    )
 }
 
 export default Home;
