78f961deae0bd656eeca7c5a54dac02004637ca7
Dibendu Saha <dibendu4@gmail.com>
2022-04-03 22:29:54 +0530

    All tech icons to a separate component

---
+++ src/components/about/tech-skills/IconsTech.jsx
@@ -0,0 +1,53 @@
+import html_icon from "../../../assets/images/icon-html.svg";
+import css_icon from "../../../assets/images/icon-css3.svg";
+import js_icon from "../../../assets/images/icon-javascript.svg";
+import jquery_icon from "../../../assets/images/icon-jquery.svg";
+import react_icon from "../../../assets/images/icon-react.svg";
+import redux_icon from "../../../assets/images/icon-redux.svg";
+import netcore_icon from "../../../assets/images/icon-dotnet-core.svg";
+import csharp_icon from "../../../assets/images/icon-c-sharp.svg";
+import api_icon from "../../../assets/images/icon-api.svg";
+import db_icon from "../../../assets/images/icon-database.svg";
+
+import IconTile from "./IconTile";
+
+function IconsTech() {
+    return (
+        <>
+            <div>
+                <IconTile icon={html_icon} title="HTML5" />
+            </div>
+            <div>
+                <IconTile icon={css_icon} title="CSS3" />
+            </div>
+            <div>
+                <IconTile icon={js_icon} title="JavaScript" />
+            </div>
+            <div>
+                <IconTile icon={jquery_icon} title="jQuery" />
+            </div>
+
+            <div>
+                <IconTile icon={react_icon} title="React" />
+            </div>
+            <div>
+                <IconTile icon={redux_icon} title="Redux" />
+            </div>
+            <div>
+                <IconTile icon={netcore_icon} title=".NET Core" />
+            </div>
+            <div>
+                <IconTile icon={csharp_icon} title="C#" />
+            </div>
+
+            <div className="grid-item-col-2">
+                <IconTile icon={api_icon} title="Web API" />
+            </div>
+            <div className="grid-item-col-3">
+                <IconTile icon={db_icon} title="SQL Server" />
+            </div>
+        </>
+    )
+}
+
+export default IconsTech;
--- src/components/about/tech-skills/TechSkills.jsx
+++ src/components/about/tech-skills/TechSkills.jsx
@@ -1,19 +1,6 @@
 import { Section, Container, H1 } from "../../common/wrapper/WrapperComponents";
 import Content from "./Content";
-
-import html_icon from "../../../assets/images/icon-html.svg";
-import css_icon from "../../../assets/images/icon-css3.svg";
-import js_icon from "../../../assets/images/icon-javascript.svg";
-import jquery_icon from "../../../assets/images/icon-jquery.svg";
-import react_icon from "../../../assets/images/icon-react.svg";
-import redux_icon from "../../../assets/images/icon-redux.svg";
-
-import netcore_icon from "../../../assets/images/icon-dotnet-core.svg";
-import csharp_icon from "../../../assets/images/icon-c-sharp.svg";
-import api_icon from "../../../assets/images/icon-api.svg";
-import db_icon from "../../../assets/images/icon-database.svg";
-
-import IconTile from "./IconTile";
+import IconsTech from "./IconsTech";
 import "./TechSkills.css";
 
 function TechSkills() {
@@ -26,38 +13,7 @@ function TechSkills() {
                         <Content />
                     </div>
                     <div className="grid grid--cols-4 grid--skills">
-                        <div>
-                            <IconTile icon={html_icon} title="HTML5" />
-                        </div>
-                        <div>
-                            <IconTile icon={css_icon} title="CSS3" />
-                        </div>
-                        <div>
-                            <IconTile icon={js_icon} title="JavaScript" />
-                        </div>
-                        <div>
-                            <IconTile icon={jquery_icon} title="jQuery" />
-                        </div>
-
-                        <div>
-                            <IconTile icon={react_icon} title="React" />
-                        </div>
-                        <div>
-                            <IconTile icon={redux_icon} title="Redux" />
-                        </div>
-                        <div>
-                            <IconTile icon={netcore_icon} title=".NET Core" />
-                        </div>
-                        <div>
-                            <IconTile icon={csharp_icon} title="C#" />
-                        </div>
-
-                        <div className="grid-item-col-2">
-                            <IconTile icon={api_icon} title="Web API" />
-                        </div>
-                        <div className="grid-item-col-3">
-                            <IconTile icon={db_icon} title="SQL Server" />
-                        </div>
+                        <IconsTech />
                     </div>
                 </div>
             </Container>
