6035c7e3caee93029514fc8f22c9296dad6dee91
Dibendu Saha <dibendu4@gmail.com>
2022-04-20 18:02:52 +0530

    'Tech Skills' paragraph margins

--- src/components/about/tech-skills/Content.jsx
+++ src/components/about/tech-skills/Content.jsx
@@ -5,20 +5,20 @@ function Content() {
                 I'm a full-stack developer, by profession. I've around 8 years of industry experience,
                 working on both the frontend and backend technnology.
             </p>
-            <br />
+
             <p>
                 I was mostly into the backend jargons fiddling with the server codes in C#, Entity Framework and the
                 Web API other than the usual jQuery but since I'm passionate about web design, graphics and design in
                 general, I managed to get my hands dirty in the world of UI/UX.
             </p>
-            <br />
+
             <p>
                 Fast forward to 2018, when the world was already moving at lightning speed, I was finally
                 able to catch up &#8212; working excluively on the frontend tech stack &#8212; HTML5, CSS3,
                 JavaScript and React. Yes, I was finally a part of the exclusive club where pages
                 were not getting generated from server-side codes anymore! (you know what I mean...)
             </p>
-            <br />
+
             <p>
                 At present, I'm working as a full-stack developer working on the modern tech stack &#8212;
                 <strong>HTML5, CSS3, React</strong> for the frontend and <strong>C#, ASP.NET Core, Web API</strong> for the server-side.
