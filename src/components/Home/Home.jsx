84eca2909e77a79c9336cd6d47d5ea73859f9705
Dibendu Saha <dibendu4@gmail.com>
2022-04-21 00:11:50 +0530

    Reveal Animation in Home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,16 +1,29 @@
+import { useEffect, useRef, useState } from "react";
+import { useReveal } from "../common/hooks/AppHooks";
 import { Section, Container, H1, Flex, Grid, Button } from "../common/wrapper/AppComponents";
 import "./Home.css";
 
 function Home() {
+    const refSection = useRef();
+    const refHeader = useRef();
+    const refContent = useRef();
+
+    const [_, setDummy] = useState("");
+
+    useEffect(() => setDummy("...to trigger a re-render"), []);
+
+    useReveal(refSection.current, refHeader.current);
+    useReveal(refSection.current, refContent.current);
+
     return (
-        <Section bgLinearGradientDegree="120">
+        <Section bgLinearGradientDegree="120" reactRef={refSection}>
             <Container>
                 <Grid className="grid--intro">
-                    <div className="intro-me">
+                    <div className="intro-me reveal-header" ref={refHeader}>
                         <H1>Hey there!</H1>
                         <H1 weight="light">I'm Dibendu</H1>
                     </div>
-                    <Flex className="flex--profession grid--intro-about">
+                    <Flex className="flex--profession grid--intro-about reveal-content" reactRef={refContent}>
                         {/* <p>...and I'm a</p>
                         <p>Full Stack Developer</p>
                         <p>by profession</p> */}
