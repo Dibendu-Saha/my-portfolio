c8720ee7c90e7621fc5fb6c393af20e468176556
Dibendu Saha <dibendu4@gmail.com>
2022-04-23 01:06:29 +0530

    Removed xx- components and references

--- src/components/landing-component/LandingComponent.jsx
+++ src/components/landing-component/LandingComponent.jsx
@@ -1,7 +1,6 @@
 import Home from "../home/Home";
 import AboutMe from "../about/about-me/AboutMe";
 import TechSkills from "../about/tech-skills/TechSkills";
-import PersonalDetails from "../xx-about-details/personal-details/PersonalDetails";
 import ContactMe from "../contact-me/ContactMe";
 
 function LandingComponent() {
