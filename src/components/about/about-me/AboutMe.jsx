77116070bf46246cc2611cbff0d4eb2da8c96e1b
Dibendu Saha <dibendu4@gmail.com>
2022-05-03 02:45:40 +0530

    Carousel slower interval

--- src/components/about/about-me/AboutMe.jsx
+++ src/components/about/about-me/AboutMe.jsx
@@ -49,6 +49,7 @@ function AboutMe() {
                             showStatus={false}
                             autoPlay={true}
                             infiniteLoop={true}
+                            interval={5000}
                             transitionTime={800}
                         >
                             <img src={profilePhoto} alt="portfolio 1" />
