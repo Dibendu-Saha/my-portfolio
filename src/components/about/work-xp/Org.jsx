52d8cb25e4dcaedfc0e292257a61fe43982b7cbc
Dibendu Saha <dibendu4@gmail.com>
2022-05-04 21:55:39 +0530

    Work Exp | WIP

--- src/components/about/work-xp/Org.jsx
+++ src/components/about/work-xp/Org.jsx
@@ -2,13 +2,13 @@ import { Flex } from "../../common/wrapper/AppComponents";
 
 function Org({ children, logo, orgName, startMonth, endMonth }) {
     return (
-        <Flex className="flex--org-wrapper border-red1">
-            <Flex className="flex--logo-wrapper border1">
-                <img src={logo} alt={logo} className="border-green1" />
+        <Flex className="flex--org-wrapper">
+            <Flex className="flex--logo-wrapper">
+                <img src={logo} alt={logo} />
             </Flex>
             <div className="work-summary">
                 <div className="summary-header">
-                    {/* <h2 className="org-title">{orgName}</h2> */}
+                    <h2 className="org-title">{orgName}</h2>
                     <h2 className="org-duration">{`${startMonth} - ${endMonth}`}</h2>
                 </div>
                 <p>
--- src/components/about/work-xp/WorkXp.jsx
+++ src/components/about/work-xp/WorkXp.jsx
@@ -11,32 +11,34 @@ function WorkXp() {
                 <H1 weight="light" className="reveal-header1111" >
                     Work experience
                 </H1>
-                <Grid col="2" className="">
-                    <Flex className="flex--xp-column">
-                        <Org
-                            logo={hpe_logo}
-                            orgName="Hewlett Packard Enterprise"
-                            startMonth="October 2021"
-                            endMonth="Present"
-                        >
-                            Deputed to client location &#8212; Hewlett Packard Enterprise, working as a full-stack developer on an
-                            internal application responsible for creating and managing “Statements of Work” that captures
-                            all projects related details.
-                        </Org>
 
-                        <Org
-                            logo={wf_logo}
-                            orgName="Wells Fargo"
-                            startMonth="April 2018"
-                            endMonth="August 2021"
-                        >
-                            Primarily worked on the front-end tech stack for a trading-based application, responsible for
-                            designing and developing the UI/UX components based on Wells Fargo&#39;s proprietary web
-                            framework along with React, JavaScript, HTML and CSS. Prior to this, I've worked as a
-                            back-end developer, developing app codes using C#, ASP.NET MVC, LINQ, Entity Framework and Web API.
-                        </Org>
-                    </Flex>
-                </Grid>
+                <div>
+                    <Org
+                        logo={hpe_logo}
+                        orgName="Hewlett Packard Enterprise"
+                        startMonth="October 2021"
+                        endMonth="Present"
+                    >
+                        Deputed to client location &#8212; Hewlett Packard Enterprise, working as a full-stack developer on an
+                        internal application responsible for creating and managing “Statements of Work” that captures all projects
+                        related details including but not limited to resources, costing and billing required for various clients.
+                        The application is based on React and Redux for its frontend tech stack while the API is based on the .NET
+                        core framework.
+                    </Org>
+
+                    <Org
+                        logo={wf_logo}
+                        orgName="Wells Fargo"
+                        startMonth="April 2018"
+                        endMonth="August 2021"
+                    >
+                        Primarily worked on the front-end tech stack on a trading-based application, responsible for
+                        designing and developing the UI/UX components based on Wells Fargo&#39;s proprietary web
+                        framework along with React, JavaScript, HTML and CSS. Prior to this, I've mainly worked as a
+                        back-end developer, developing app codes using C#, ASP.NET MVC, LINQ, Entity Framework and Web API.
+                    </Org>
+                </div>
+
             </Container>
         </Section>
     )
