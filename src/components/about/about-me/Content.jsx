aeeb196ec61082ed20ec19588835a9c4ddb82455
Dibendu Saha <dibendu4@gmail.com>
2022-05-06 20:56:59 +0530

    'About Me' and 'Tech Space' content revamp

--- src/components/about/about-me/Content.jsx
+++ src/components/about/about-me/Content.jsx
@@ -4,63 +4,21 @@ import icon_new_window from "../../../assets/images/icon-new-window.png";
 function Content() {
     return (
         <>
-            <>
-                {/* <Card height="15rem" width="30rem" style="dark">
-                <p>Hi, I'm Dibendu Saha.</p>
-            </Card>
-
-            <Card height="15rem" width="30rem">
-                Crash-landed on earth around three decades back,
-                somewhere in the year 1990...
-            </Card>
-
-            <Card height="15rem" width="30rem" style="dark">
-                ...and the crash site? Far in North-east India, in the state of Assam!
-            </Card>
-
-            <Card height="15rem" width="30rem">
-                I love to Sketch. Capture shots. Ride.
-                Oh and yes. Cook. Big Fan of Jamie Oliver (alright. alright. Gordon Ramsay too!)
-            </Card>
-
-            <Card height="15rem" width="30rem" style="dark">
-                Graduated with a Bachelor's degree in
-                Electronics &amp; Communication, yet landed up in IT
-                (it's still a good thing).
-            </Card>
-
-            <div className="btn-wrapper">
-                <Card height="15rem" width="30rem">
-                    <Button
-                        title="Know about my professional experience..."
-                        type="none"
-                        link="/work"
-                    />
-                </Card>
-            </div> */}
-            </>
-
             <p>
-                Hi, I'm <Highlight>Dibendu Saha</Highlight>. I hail from the northeast Indian state of Assam and currently
+                Hi, I'm <Highlight>Dibendu Saha</Highlight>. I'm from the northeast Indian state of Assam and currently
                 based in Bangalore.
             </p>
 
             <p>
-                I design web user interfaces and hook them up with backend API's to build complete data-driven solutions
-                for end-clients. I've worked with organizations like <Highlight>Wells Fargo</Highlight> and had offers from
-                the likes of <Highlight>EY, Danske Bank</Highlight> and <Highlight>Siemens</Highlight>. Currently I'm a
-                Technical Consultant at <Highlight>Hewlett Packard Enterprise</Highlight>.
+                I moved to Bangalore to pursue higher education in engineering in the field of Electronics &amp; Communication,
+                graduating and thus landed my first job as a Software Engineer Trainee in the city. Yay!
             </p>
 
             <p>
-                I've earned quite a few recognitions for my work and the successful release cycles. I also hold a certification
-                in <a href="https://www.credly.com/badges/3c6991e8-43ec-4dc5-8006-d19ec9a456e6" target="_blank"><span className="link-text">Azure Fundamentals</span> <span><img src={icon_new_window} alt="Open in new window icon" className="icon-new-window" /></span></a> from Microsoft.
-            </p>
-
-            <p>
-                I can speak four languages &#8212; English, Hindi, Assamese and Bengali &#8212; my native-tongue.
-                Enjoy cooking to make wholesome foods for my loved ones. I'm also into biking and occasionally nowhere to be found on weekends.
-                I'm part of a professional rider's groups. So, both solo trips and group rides.
+                Been in the city since, enough to call it my (second) home. I speak four languages &#8212; English, Hindi, Assamese
+                and Bengali &#8212; my native-tongue. Enjoy cooking to make wholesome foods for my loved ones. I'm also into biking
+                and occasionally nowhere to be found on weekends. I'm part of a professional rider's groups. So, both solo trips
+                and group rides.
             </p>
         </>
     )
--- src/components/about/tech-skills/Content.jsx
+++ src/components/about/tech-skills/Content.jsx
@@ -4,20 +4,22 @@ function Content() {
     return (
         <>
             <p>
-                I'm a full-stack developer by profession and have around 8 years of industry experience.
+                I'm a <Highlight>full-stack developer</Highlight> by profession and have around 8 years
+                of industry experience. I design web user interfaces and hook them up with backend API's
+                to build complete data-driven solutions for end-clients.
             </p>
 
             <p>
-                I was mainly into the backend jargons developing Web API's in <Highlight>C#</Highlight> and <Highlight>ASP.NET</Highlight> other
-                than the usual jQuery but since I'm passionate about web design, graphics and design in general, I managed to get my
-                hands dirty into the world of UI and UX &#8212; working exclusively on the frontend tech stack &#8212; <Highlight>HTML5, CSS3, JavaScript</Highlight>
-                and <Highlight>React</Highlight>. Yes, I was finally a part of the exclusive club where pages
-                were not getting generated from server-side codes anymore! (you know what I mean...)
+                I was mainly into the backend jargons developing Web API's in C# and ASP.NET other
+                than the usual jQuery but since I'm passionate about web design, graphics and design in general,
+                I managed to get my hands dirty into the world of UI and UX &#8212; working exclusively on the
+                frontend tech stack &#8212; HTML5, CSS3, JavaScript and React.
             </p>
 
             <p>
-                At present, I'm working as a full-stack developer, developing user interface components
-                with HTML5, CSS3, React, and the Web API with C#, ASP.NET Core and SQL Server.
+                At present, I'm a <Highlight>Technical Consultant</Highlight> at <Highlight>Hewlett Packard Enterprise</Highlight>, where I work as a full-stack
+                developer, developing user interface components with <Highlight>HTML5, CSS3, React</Highlight> and connect it with the
+                Web API with <Highlight>C#, ASP.NET Core</Highlight> and <Highlight>SQL Server</Highlight>.
             </p>
         </>
     )
