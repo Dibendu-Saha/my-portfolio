8ad66e14ee4bce368f9f06145cb6787baaa2185a
Dibendu Saha <dibendu4@gmail.com>
2022-05-07 16:29:06 +0530

    Tech skills content modifications

--- src/components/about/tech-skills/Content.jsx
+++ src/components/about/tech-skills/Content.jsx
@@ -10,7 +10,7 @@ function Content() {
             </p>
 
             <p>
-                I was mainly into the backend jargons developing Web API's in C# and ASP.NET other
+                I was mainly into backend programming developing Web API's in C# and ASP.NET other
                 than the usual jQuery but since I'm passionate about web design, graphics and design in general,
                 I managed to get my hands dirty into the world of UI and UX &#8212; working exclusively on the
                 frontend tech stack &#8212; HTML5, CSS3, JavaScript and React.
@@ -26,9 +26,9 @@ function Content() {
             </p>
 
             <p>
-                At present, I'm a <Highlight>Technical Consultant</Highlight> at <Highlight>Hewlett Packard Enterprise</Highlight>, where I work as a full-stack
-                developer, developing user interface components with <Highlight>HTML5, CSS3, React</Highlight> and connect it with the
-                Web API with <Highlight>C#, ASP.NET Core</Highlight> and <Highlight>SQL Server</Highlight>.
+                At present, I'm a <Highlight>Technical Consultant</Highlight> at <Highlight>Hewlett Packard Enterprise</Highlight>,
+                where I work as a full-stack developer, developing user interface components with <Highlight>HTML5, CSS3, React</Highlight> and
+                connect it via the Web API using <Highlight>C#, ASP.NET Core</Highlight> and <Highlight>SQL Server</Highlight>.
             </p>
         </>
     )
