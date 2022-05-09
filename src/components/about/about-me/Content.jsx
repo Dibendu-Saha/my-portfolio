dc03719cf7a46e7c4336be799c3022cf1806ebe6
Dibendu Saha <dibendu4@gmail.com>
2022-05-10 00:55:36 +0530

    'About Me' content changed

--- src/components/about/about-me/Content.jsx
+++ src/components/about/about-me/Content.jsx
@@ -9,15 +9,13 @@ function Content() {
             </p>
 
             <p>
-                I moved to Bangalore to pursue higher education in engineering in the field of Electronics &amp; Communication,
-                graduating and thus landed my first job as a Software Engineer Trainee in the city. Yay!
+                I speak four languages &#8212; English, Hindi, Assamese and Bengali being my native-tongue. Enjoy cooking and
+                trying out recipes for my loved ones, as I believe nothing else can touch a soul more than wholesome foods.
             </p>
 
             <p>
-                Been in the city since, enough to call it my (second) home. I speak four languages &#8212; English, Hindi, Assamese
-                and Bengali &#8212; my native-tongue. Enjoy cooking to make wholesome foods for my loved ones. I'm also into biking
-                and occasionally nowhere to be found on weekends. I'm part of a professional rider's groups. So, both solo trips
-                and group rides.
+                I love travelling <i>(...I mean who doesn't)</i>, am a photography enthusiast and into biking and occasionally nowhere to
+                be found on weekends. I'm also part of a professional rider's groups. So, I go for both solo trips and group rides.
             </p>
         </>
     )
